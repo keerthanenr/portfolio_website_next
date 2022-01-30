ba2199422716ee8672831074a73b45fa7e4d8b49
Keerthanen Ravichandran <keerthanenravichandran@keerthanens-mbp.home>
2022-01-30 21:30:16 +0000

    new website for fun

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import { ThemeProvider } from 'next-themes';
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <>
+    <ThemeProvider attribute="class" enableSystem={true}>
+      <Navigation/>
+      <Component {...pageProps} />
+      </ThemeProvider>
+      <Footer/>
+    </>
+  )
+}
+
+export default MyApp
